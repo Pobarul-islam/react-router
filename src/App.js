e8508e114f8d19efd7e0449d482f6aa9c7ce76a2
Pobarul-islam <pobarul3@gmail.com>
2022-03-29 20:46:43 +0600

    rest country added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import Countries from './components/Countries/Countries';
+import CountryDetail from './components/CountryDetail/CountryDetail';
 import Friend from './components/Friend/Friend';
 import FriendDetail from './components/FriendDetail/FriendDetail';
 import Friends from './components/Friends/Friends';
@@ -17,14 +19,16 @@ function App() {
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/friends' element={<Friends></Friends>}></Route>
-        <Route path='/friend/:friendId' element={<FriendDetail></FriendDetail>}></Route>
-        <Route path='posts' element={<Posts></Posts>}>
-          <Route path=':postId' element={<PostDetail></PostDetail>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/friends" element={<Friends></Friends>}></Route>
+        <Route path="/friend/:friendId" element={<FriendDetail></FriendDetail>}></Route>
+        <Route path="posts" element={<Posts></Posts>}>
+          <Route path=":postId" element={<PostDetail></PostDetail>}></Route>
         </Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path="/countries" element={<Countries></Countries>}></Route>
+        <Route path="/country/:countryName" element={<CountryDetail></CountryDetail>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
         <Link to="/home">Home</Link>
         <Link to="/friends">Friends</Link>
         <Link to="/posts">Posts</Link>
+        <Link to="/countries">Countries</Link>
         <Link to="/about">About</Link>
         {/* <CoustomLink to="/home">Home</CoustomLink>
         <CoustomLink to="/friends">Friends</CoustomLink>
