9be2ca7169787ff72a18bb4dd0dbc4da5f3b3593
Pobarul-islam <pobarul3@gmail.com>
2022-03-29 16:23:34 +0600

    react router code added

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is About us</h2>
+            <p>Wanna know about us?</p>
+            <p><small>Taile age biriani khawa.. na khawaile bolbona</small></p>
+
+        </div>
+    );
+};
+
+export default About;
